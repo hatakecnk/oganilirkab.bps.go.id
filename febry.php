551642fc10a29d89f20b9c503a51b209962918f1
xNot_Found <41625152+hatakecnk@users.noreply.github.com>
2019-06-23 00:20:13 +0700

    Add files via upload

---
+++ febry.php
@@ -0,0 +1,562 @@
+<head>
+  <title>Pwnd?</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Owned by Indonesian People">
+  <meta name="keywords" content="Hacked">
+  <meta name="author" content="./xNot_Found">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta property="og:image" content="https://i.ibb.co/RQQrmcb/Pics-Art-05-17-12-55-53.png">
+</head>
+<span id="sec"><div id="foter" class="foter" style="position: absolute; top: 20px; left: -225px; width: 500px; height: 20px; margin: 0px; padding: 10px; font-size: 20px; text-align: center; color: rgb(255, 255, 255); font-family: &quot;trebuchet ms&quot;, Courier new, courier new, sans-serif; transform: rotate(-45deg); transform-origin: 50% 0px 0px; background-color: rgb(0, 0, 0); border: 1px solid rgb(170, 170, 170); opacity: 0.5; z-index: 1200;">
+<marquee color="darkblue" behavior="Flip" scrollamount="5" width="50%" style="width: 50%;">No System Is Safe</marquee></div></span>
+<iframe width="0" height="0" src="https://www.youtube.com/embed/1-xGerv5FOk?autoplay=1" frameborder="0" allowfullscreen=""></iframe>
+<br><br>
+<script type='text/javascript'>
+//<![CDATA[
+;(function(){
+var msg = "./xNot_Found Was Here";
+var size = 10;
+var circleY = 0.75; var circleX = 2;
+var letter_spacing = 5;
+var diameter = 10;
+var rotation = 0.2;
+var speed = 0.3;
+if (!window.addEventListener && !window.attachEvent || !document.createElement) return;
+msg = msg.split('');
+var n = msg.length - 1, a = Math.round(size * diameter * 0.208333), currStep = 20,
+ymouse = a * circleY + 20, xmouse = a * circleX + 20, y = [], x = [], Y = [], X = [],
+o = document.createElement('div'), oi = document.createElement('div'),
+b = document.compatMode && document.compatMode != "BackCompat"? document.documentElement :
+document.body,
+mouse = function(e){
+e = e || window.event;
+ymouse = !isNaN(e.pageY)? e.pageY : e.clientY; // y-position
+xmouse = !isNaN(e.pageX)? e.pageX : e.clientX; // x-position
+},
+makecircle = function(){ // rotation/positioning
+if(init.nopy){
+o.style.top = (b || document.body).scrollTop + 'px';
+o.style.left = (b || document.body).scrollLeft + 'px';
+};
+currStep -= rotation;
+for (var d, i = n; i > -1; --i){ // makes the circle
+d = document.getElementById('iemsg' + i).style;
+d.top = Math.round(y[i] + a * Math.sin((currStep + i) / letter_spacing) * circleY - 15) +
+'px';
+d.left = Math.round(x[i] + a * Math.cos((currStep + i) / letter_spacing) * circleX) + 'px';
+};
+},
+drag = function(){ // makes the resistance
+y[0] = Y[0] += (ymouse - Y[0]) * speed;
+x[0] = X[0] += (xmouse - 20 - X[0]) * speed;
+for (var i = n; i > 0; --i){
+y[i] = Y[i] += (y[i-1] - Y[i]) * speed;
+x[i] = X[i] += (x[i-1] - X[i]) * speed;
+};
+makecircle();
+},
+init = function(){ // appends message divs, & sets initial values for positioning arrays
+if(!isNaN(window.pageYOffset)){
+ymouse += window.pageYOffset;
+xmouse += window.pageXOffset;
+} else init.nopy = true;
+for (var d, i = n; i > -1; --i){
+d = document.createElement('div'); d.id = 'iemsg' + i;
+d.style.height = d.style.width = a + 'px';
+d.appendChild(document.createTextNode(msg[i]));
+oi.appendChild(d); y[i] = x[i] = Y[i] = X[i] = 0;
+};
+o.appendChild(oi); document.body.appendChild(o);
+setInterval(drag, 25);
+},
+ascroll = function(){
+ymouse += window.pageYOffset;
+xmouse += window.pageXOffset;
+window.removeEventListener('scroll', ascroll, false);
+};
+o.id = 'outerCircleText'; o.style.fontSize = size + 'px';
+if (window.addEventListener){
+window.addEventListener('load', init, false);
+document.addEventListener('mouseover', mouse, false);
+document.addEventListener('mousemove', mouse, false);
+if (/Apple/.test(navigator.vendor))
+window.addEventListener('scroll', ascroll, false);
+}
+else if (window.attachEvent){
+window.attachEvent('onload', init);
+document.attachEvent('onmousemove', mouse);
+};
+})();
+//]]>
+</script>
+<style type='text/css'>
+#outerCircleText {
+font-style: italic;
+font-weight: bold;
+font-family: &#39;comic sans ms&#39;, verdana, arial;
+color: aqua;
+position: absolute;top: 0;left: 0;z-index: 3000;cursor: default;}
+#outerCircleText div {position: relative;}
+#outerCircleText div div {position: absolute;top: 0;left: 0;text-align: center;}
+</style>
+<script>
+     alert('Hello');
+     alert('xNot_Found Was Here');
+     alert('Sorry Admin, I Just Testing Your System')
+</script>
+<style>
+font{
+  font-familly:Orbitron;
+  text-align:center;
+  letter-spacing:1px;
+  color:white;
+  /*Create overlap*/
+  
+  margin:0;
+  line-height:0;
+  /*Animation*/
+  
+  -webkit-animation: glitch1 2.5s infinite;
+  
+          animation: glitch1 2.5s infinite;
+}
+font:nth-child(2) {
+  color: #67f3da;
+  -webkit-animation: glitch2 2.5s infinite;
+          animation: glitch2 2.5s infinite;
+}
+
+font:nth-child(3) {
+  color: #f16f6f;
+  -webkit-animation: glitch3 2.5s infinite;
+          animation: glitch3 2.5s infinite;
+}
+/*Keyframes*/
+
+@-webkit-keyframes glitch1 {
+  0% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+  7% {
+    -webkit-transform: skew(-0.5deg, -0.9deg);
+            transform: skew(-0.5deg, -0.9deg);
+    opacity: 0.75;
+  }
+  10% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+  27% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+  30% {
+    -webkit-transform: skew(0.8deg, -0.1deg);
+            transform: skew(0.8deg, -0.1deg);
+    opacity: 0.75;
+  }
+  35% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+  52% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+  55% {
+    -webkit-transform: skew(-1deg, 0.2deg);
+            transform: skew(-1deg, 0.2deg);
+    opacity: 0.75;
+  }
+  50% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+  72% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+  75% {
+    -webkit-transform: skew(0.4deg, 1deg);
+            transform: skew(0.4deg, 1deg);
+    opacity: 0.75;
+  }
+  80% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+  100% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+}
+
+@keyframes glitch1 {
+  0% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+  7% {
+    -webkit-transform: skew(-0.5deg, -0.9deg);
+            transform: skew(-0.5deg, -0.9deg);
+    opacity: 0.75;
+  }
+  10% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+  27% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+  30% {
+    -webkit-transform: skew(0.8deg, -0.1deg);
+            transform: skew(0.8deg, -0.1deg);
+    opacity: 0.75;
+  }
+  35% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+  52% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+  55% {
+    -webkit-transform: skew(-1deg, 0.2deg);
+            transform: skew(-1deg, 0.2deg);
+    opacity: 0.75;
+  }
+  50% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+  72% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+  75% {
+    -webkit-transform: skew(0.4deg, 1deg);
+            transform: skew(0.4deg, 1deg);
+    opacity: 0.75;
+  }
+  80% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+  100% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 1;
+  }
+}
+
+@-webkit-keyframes glitch2 {
+  0% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  7% {
+    -webkit-transform: translate(-2px, -3px);
+            transform: translate(-2px, -3px);
+    opacity: 0.5;
+  }
+  10% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  27% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  30% {
+    -webkit-transform: translate(-5px, -2px);
+            transform: translate(-5px, -2px);
+    opacity: 0.5;
+  }
+  35% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  52% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  55% {
+    -webkit-transform: translate(-5px, -1px);
+            transform: translate(-5px, -1px);
+    opacity: 0.5;
+  }
+  50% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  72% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  75% {
+    -webkit-transform: translate(-2px, -6px);
+            transform: translate(-2px, -6px);
+    opacity: 0.5;
+  }
+  80% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  100% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+}
+
+@keyframes glitch2 {
+  0% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  7% {
+    -webkit-transform: translate(-2px, -3px);
+            transform: translate(-2px, -3px);
+    opacity: 0.5;
+  }
+  10% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  27% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  30% {
+    -webkit-transform: translate(-5px, -2px);
+            transform: translate(-5px, -2px);
+    opacity: 0.5;
+  }
+  35% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  52% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  55% {
+    -webkit-transform: translate(-5px, -1px);
+            transform: translate(-5px, -1px);
+    opacity: 0.5;
+  }
+  50% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  72% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  75% {
+    -webkit-transform: translate(-2px, -6px);
+            transform: translate(-2px, -6px);
+    opacity: 0.5;
+  }
+  80% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  100% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+}
+mm
+@-webkit-keyframes glitch3 {
+  0% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  7% {
+    -webkit-transform: translate(2px, 3px);
+            transform: translate(2px, 3px);
+    opacity: 0.5;
+  }
+  10% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  27% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  30% {
+    -webkit-transform: translate(5px, 2px);
+            transform: translate(5px, 2px);
+    opacity: 0.5;
+  }
+  35% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  52% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  55% {
+    -webkit-transform: translate(5px, 1px);
+            transform: translate(5px, 1px);
+    opacity: 0.5;
+  }
+  50% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  72% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  75% {
+    -webkit-transform: translate(2px, 6px);
+            transform: translate(2px, 6px);
+    opacity: 0.5;
+  }
+  80% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  100% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+}
+
+@keyframes glitch3 {
+  0% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  7% {
+    -webkit-transform: translate(2px, 3px);
+            transform: translate(2px, 3px);
+    opacity: 0.5;
+  }
+  10% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  27% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  30% {
+    -webkit-transform: translate(5px, 2px);
+            transform: translate(5px, 2px);
+    opacity: 0.5;
+  }
+  35% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  52% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  55% {
+    -webkit-transform: translate(5px, 1px);
+            transform: translate(5px, 1px);
+    opacity: 0.5;
+  }
+  50% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  72% {
+    -webkit-transform: none;
+            transform: none;
+    opacity: 0.25;
+  }
+  75% {
+    -webkit-transform: translate(2px, 6px);
+            transform: translate(2px, 6px);
+    opacity: 0.5;
+  }
+  80% {
+    -webkit-transform: none;transform: none;opacity: 0.25;}100% 
+{
+    -webkit-transform: none;transform: 
+         none;opacity: 0.25;
+  }
+ }
+@keyframes kedipz {0%{opacity:0}10%{opacity:1}20%{opacity:0}30%{opacity:1}40%{opacity:0}50%{opacity:1}60%{opacity:0}70%{opacity:1}80%{opacity:0}90%{opacity:1}95%{opacity:0}100%{opacity:1}}a {color:#27f6a4;text-decoration:none;}a:hover, a:active {opacity:1;animation-name:kedipz;animation-duration:0.5s}.barlink {margin-bottom:10px;}.justified {margin-top:15px;margin-bottom:20px;border-top:3px #27f6a4 solid;border-bottom:3px #27f6a4 solid;padding:5px;padding-top:10px;padding-bottom:10px;text-align: justify;-moz-text-align-last: center;text-align-last: center;width:80%;}.containerx {width: 60%;text-align: center;margin-top:15px;margin-bottom:20px;border-top:3px #27f6a4 solid;border-bottom:3px #27f6a4 solid;padding:10px;padding-top:10px;padding-bottom:10px;}.contentx {text-align: justify;}h2 {margin:0;}@media screen and (max-width: 500px) {.containerx {width: 80%;}}
+body{background-color:black;font-family: 'Share Tech Mono', monospace;}
+<link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono" rel="stylesheet">
+<link href="https://i.ibb.co/RQQrmcb/Pics-Art-05-17-12-55-53.png" rel="shortcut icon">
+</style>
+<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+<body>
+<center>
+<br>
+  <img src="https://i.ibb.co/RQQrmcb/Pics-Art-05-17-12-55-53.png" width="200" heigth="100"/>
+  <br>
+  <br>
+  <br>
+     <font color="white" size="4" face="Orbitron">Hacked By ./xNot_Found</font><br>
+     <footer>
+     <table style="border:1px;border-color:blue;border-style:double;padding-left:2px;padding-right:2px;bottom:2px;height:25px;width:100%;">
+     <td><center><a color="blue">“The way to get started is to quit talking and begin doing.”</td>
+<br><br>
+<a href="https://twitter.com/hatakecnk?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-
+show-count="false">Follow Me</a><script async src="https://platform.twitter.com/widgets.js" 
+charset="utf-8"></script>
+</center>
+</body>
+<iframe width="0" height="0" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/302993013&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
